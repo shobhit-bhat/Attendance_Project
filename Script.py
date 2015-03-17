90314cb18f129a9d6da4d543cbf229d18166263a
Shobhit Bhatnagar <shobhit.bhat@gmail.com>
2015-03-17 23:21:10 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
-from att.models import Registrati11on, Attendance
+from att.models import Registration, Attendance
 import shutil
 import csv
 
 #Assuming only one file name. Will have to modify for multiple file names and figure out a way of naming.
 
 arr=csv.reader(open('Unprocessed/1086.att', 'r'),delimiter='.')
-#for row in arr:
 row=arr.next()
 a = Registration(School_ID=row[0],School_Name=row[1],Max_Students=row[2],Date=row[3])
 a.save()
-
 shutil.move("Unprocessed/1086.att", "Processed/1086.att")
